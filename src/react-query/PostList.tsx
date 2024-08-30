f036d6571b05d263b28def828577ee289472dd93
Swaraj Roy <contactswarajroy@gmail.com>
2024-08-30 17:27:15 +0100

    learnt implementing pagination

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -9,8 +9,9 @@ interface Post {
 }
 
 const PostList = () => {
-  const [userId, setUserId] = useState<number>();
-  const { data: posts, error, isLoading } = usePosts(userId);
+  const pageSize = 10;
+  const [page, setPage] = useState<number>(1);
+  const { data: posts, error, isLoading } = usePosts({ page, pageSize });
 
   if (isLoading) return <p>Loading...</p>;
 
@@ -18,17 +19,6 @@ const PostList = () => {
 
   return (
     <>
-      <select
-        onChange={(event) => setUserId(parseInt(event.target.value))}
-        className="form-select mb-3"
-        value={userId}
-      >
-        <option value=""></option>
-        <option value="1">User 1</option>
-        <option value="2">User 2</option>
-        <option value="3">User 3</option>
-      </select>
-
       <ul className="list-group">
         {posts?.map((post) => (
           <li key={post.id} className="list-group-item">
@@ -36,6 +26,19 @@ const PostList = () => {
           </li>
         ))}
       </ul>
+      <button
+        disabled={page === 1}
+        onClick={() => setPage(page - 1)}
+        className="btn btn-primary my-3"
+      >
+        Prev
+      </button>
+      <button
+        onClick={() => setPage(page + 1)}
+        className="btn btn-primary my-3 ms-1"
+      >
+        Next
+      </button>
     </>
   );
 };
