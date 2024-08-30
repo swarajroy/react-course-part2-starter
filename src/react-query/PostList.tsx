0c069ba8dffa04689e98b26b14f5d597826135bc
Swaraj Roy <contactswarajroy@gmail.com>
2024-08-30 13:02:18 +0100

    initial commit and learning react query

---
+++ src/react-query/PostList.tsx
@@ -0,0 +1,28 @@
+import usePosts from "./hooks/usePosts";
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
+
+const PostList = () => {
+  const { data: posts, error, isLoading } = usePosts();
+
+  if (isLoading) return <p>Loading...</p>;
+
+  if (error) return <p>{error.message}</p>;
+
+  return (
+    <ul className="list-group">
+      {posts?.map((post) => (
+        <li key={post.id} className="list-group-item">
+          {post.title}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default PostList;
