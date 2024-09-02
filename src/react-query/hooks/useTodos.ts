d7185288fbbab6532132efb9565107f55d4e9342
Swaraj Roy <contactswarajroy@gmail.com>
2024-09-02 18:21:33 +0100

    generic api client implemented

--- src/react-query/hooks/useTodos.ts
+++ src/react-query/hooks/useTodos.ts
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import APIClient from "../services/apiClient";
 import CACHE_KEY_TODOS from "../constants";
 
 export interface Todo {
@@ -9,15 +9,12 @@ export interface Todo {
   completed: boolean;
 }
 
-const useTodos = () => {
-  const fetchTodos = () =>
-    axios
-      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
-      .then((res) => res.data);
+const apiClient = new APIClient<Todo>("/todos");
 
+const useTodos = () => {
   return useQuery<Todo[], Error>({
     queryKey: CACHE_KEY_TODOS,
-    queryFn: fetchTodos,
+    queryFn: apiClient.getAll,
     staleTime: 10 * 1000,
   });
 };
