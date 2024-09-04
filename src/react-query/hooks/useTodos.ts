99defd71be6972fd95fbf147ab338bb6b31e07b4
Swaraj Roy <contactswarajroy@gmail.com>
2024-09-04 10:45:04 +0100

    services for todos introduced

--- src/react-query/hooks/useTodos.ts
+++ src/react-query/hooks/useTodos.ts
@@ -1,20 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
-import APIClient from "../services/apiClient";
+import todoService, { Todo } from "../services/todoService";
 import CACHE_KEY_TODOS from "../constants";
-
-export interface Todo {
-  id: number;
-  title: string;
-  userId: number;
-  completed: boolean;
-}
-
-const apiClient = new APIClient<Todo>("/todos");
 
 const useTodos = () => {
   return useQuery<Todo[], Error>({
     queryKey: CACHE_KEY_TODOS,
-    queryFn: apiClient.getAll,
+    queryFn: todoService.getAll,
     staleTime: 10 * 1000,
   });
 };
